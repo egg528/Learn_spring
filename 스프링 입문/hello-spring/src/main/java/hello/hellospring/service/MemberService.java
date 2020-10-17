df698504b80f54ed474bf72d5980e2ded79bf680
egg528 <62459196+egg528@users.noreply.github.com>
2020-10-17 13:03:55 +0900

    make logic about domain, service, repository

--- 스프링 입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ 스프링 입문/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     /**
      * 회원 가입
@@ -19,7 +23,6 @@ public class MemberService {
         validateDuplicateMember(member);
         memberRepository.save(member);
         return member.getId();
-        
     }
 
     private void validateDuplicateMember(Member member) {
